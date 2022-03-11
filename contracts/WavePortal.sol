2212367f153a01259ee14def4c2fe3fafdfce266
bayamasa <mapi0213mgt@gmail.com>
2022-03-11 23:12:43 +0900

    MOD: 一旦コミット

--- contracts/WavePortal.sol
+++ contracts/WavePortal.sol
@@ -8,6 +8,8 @@ contract WavePortal {
 	
 	uint256 totalWaves;
 	
+	uint256 private seed;
+	
 	event NewWave(address indexed from, uint256 timestamp, string message);
 	
 	struct Wave {
@@ -17,15 +19,41 @@ contract WavePortal {
 	}
 	
 	Wave[] waves;
-	constructor() {
-		console.log("WavePortal - Smart Contract");
+	mapping (address=>uint256) public lastWavedAt;
+	constructor() payable{
+		console.log("We have been constructed!");
+		// 初期シードを設定
+		seed = (block.timestamp + block.difficulty) % 100;
 	}
 	
 	function wave(string memory _message) public{
+		require(
+			lastWavedAt[msg.sender] < block.timestamp,
+			"Wait 15m"
+		);
+
+		lastWavedAt[msg.sender] = block.timestamp;
 		totalWaves += 1;
 		console.log("%s waved w/ message %s", msg.sender, _message);
 		
 		waves.push(Wave(msg.sender, _message, block.timestamp));
+		
+		seed = (block.difficulty + block.timestamp + seed) % 100;
+		console.log("Random # generated: %d", seed);
+		
+		if (seed <= 50) {
+			console.log("%s won!", msg.sender);
+			
+			uint256 prizeAmount = 0.001 ether;
+			require(
+				prizeAmount <= address(this).balance,
+				"Trying to withdraw more money than the contract has."
+			);
+			(bool success, ) = (msg.sender).call{value: prizeAmount}("");
+			require(success, "Failed to withdraw money from contract");
+		} else {
+			console.log("%s did not win.", msg.sender);
+		}
 		
 		emit NewWave(msg.sender, block.timestamp, _message);
 	}
