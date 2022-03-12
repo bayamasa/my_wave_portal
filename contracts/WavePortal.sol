3fd95d1472943f4a3b9cd76a28d28b8abc063555
bayamasa <mapi0213mgt@gmail.com>
2022-03-12 10:59:06 +0900

    WIP: とりあえず

--- contracts/WavePortal.sol
+++ contracts/WavePortal.sol
@@ -1,69 +1,81 @@
 // SPDX-License-Identifier: UNLICENSED
 
-pragma solidity ^0.8.4;
+pragma solidity ^0.8.0;
 
 import "hardhat/console.sol";
 
 contract WavePortal {
-	
-	uint256 totalWaves;
-	
-	uint256 private seed;
-	
-	event NewWave(address indexed from, uint256 timestamp, string message);
-	
-	struct Wave {
-		address waver;
-		string message;
-		uint256 timestamp;
-	}
-	
-	Wave[] waves;
-	mapping (address=>uint256) public lastWavedAt;
-	constructor() payable{
-		console.log("We have been constructed!");
-		// 初期シードを設定
-		seed = (block.timestamp + block.difficulty) % 100;
-	}
-	
-	function wave(string memory _message) public{
-		require(
-			lastWavedAt[msg.sender] < block.timestamp,
-			"Wait 15m"
-		);
+    uint256 totalWaves;
+    uint256 private seed;
 
-		lastWavedAt[msg.sender] = block.timestamp;
-		totalWaves += 1;
-		console.log("%s waved w/ message %s", msg.sender, _message);
-		
-		waves.push(Wave(msg.sender, _message, block.timestamp));
-		
-		seed = (block.difficulty + block.timestamp + seed) % 100;
-		console.log("Random # generated: %d", seed);
-		
-		if (seed <= 50) {
-			console.log("%s won!", msg.sender);
-			
-			uint256 prizeAmount = 0.001 ether;
-			require(
-				prizeAmount <= address(this).balance,
-				"Trying to withdraw more money than the contract has."
-			);
-			(bool success, ) = (msg.sender).call{value: prizeAmount}("");
-			require(success, "Failed to withdraw money from contract");
-		} else {
-			console.log("%s did not win.", msg.sender);
-		}
-		
-		emit NewWave(msg.sender, block.timestamp, _message);
-	}
-	
-	function getAllWaves() public view returns (Wave[] memory) {
-		return waves; 
-	}
-	
-	function getTotalWaves() public view returns (uint256) {
-		console.log("We have %d total waves!", totalWaves);
-		return totalWaves;
-	}
+    event NewWave(address indexed from, uint256 timestamp, string message);
+
+    struct Wave {
+        address waver;
+        string message;
+        uint256 timestamp;
+    }
+
+    Wave[] waves;
+
+    /*
+     * "address => uint mapping"は、アドレスと数値を関連付ける
+     */
+    mapping(address => uint256) public lastWavedAt;
+
+    constructor() payable {
+        console.log("We have been constructed!");
+        /*
+         * 初期シードの設定
+         */
+        seed = (block.timestamp + block.difficulty) % 100;
+    }
+
+    function wave(string memory _message) public {
+        /*
+         * 現在ユーザーがwaveを送信している時刻と、前回waveを送信した時刻が15分以上離れていることを確認。
+         */
+        require(
+            lastWavedAt[msg.sender] < block.timestamp,
+            "Wait 15m"
+        );
+
+        /*
+         * ユーザーの現在のタイムスタンプを更新する
+         */
+        lastWavedAt[msg.sender] = block.timestamp;
+
+        totalWaves += 1;
+        console.log("%s has waved!", msg.sender);
+
+        waves.push(Wave(msg.sender, _message, block.timestamp));
+
+        /*
+         *  ユーザーのために乱数を設定
+         */
+        seed = (block.difficulty + block.timestamp + seed) % 100;
+
+		console.log(seed);
+        if (seed <= 50) {
+            console.log("%s won!", msg.sender);
+
+            uint256 prizeAmount = 0.0001 ether;
+            require(
+                prizeAmount <= address(this).balance,
+                "Trying to withdraw more money than they contract has."
+            );
+            (bool success, ) = (msg.sender).call{value: prizeAmount}("");
+            require(success, "Failed to withdraw money from contract.");
+        }
+
+        emit NewWave(msg.sender, block.timestamp, _message);
+    }
+
+    function getAllWaves() public view returns (Wave[] memory) {
+        return waves;
+    }
+
+    function getTotalWaves() public view returns (uint256) {
+        return totalWaves;
+    }
 }
--- scripts/run.js
+++ scripts/run.js
@@ -1,17 +1,44 @@
 const main = async () => {
 	const waveContractFactory = await hre.ethers.getContractFactory("WavePortal");
-	const waveContract = await waveContractFactory.deploy();
-	console.log("Contract added to:", waveContract.address);
+	const waveContract = await waveContractFactory.deploy({
+		value: hre.ethers.utils.parseEther("0.1"),
+	});
+	await waveContract.deployed();
+	console.log("Contract deployed to: ", waveContract.address);
+
+	let contractBalance = await hre.ethers.provider.getBalance(
+		waveContract.address
+	);
+	console.log(
+		"Contract balance:",
+		hre.ethers.utils.formatEther(contractBalance)
+	);
+	contractBalance = await hre.ethers.provider.getBalance(
+		waveContract.address
+	);
+	console.log(
+		"Contract balance:",
+		hre.ethers.utils.formatEther(contractBalance)
+	);
 	
-	let waveCount;
-	waveCount = await waveContract.getTotalWaves();
-	console.log(waveCount.toNumber());
+	let waveTxn = await waveContract.wave("This is wave #1");
+	await waveTxn.wait();
 	
-	let waveTxn = await waveContract.wave("A message!");
-	await waveTxn.wait();
-	const [_, randomPerson] = await hre.ethers.getSigners();
-	waveTxn = await waveContract.connect(randomPerson).wave("Another message!");
-	await waveTxn.wait();
+	let waveTxn2 = await waveContract.wave("This is wave #1");
+	await waveTxn2.wait();
+
+	/*	
+	* コントラクトの残高を取得し、Waveを取得した後の結果を出力
+	*/
+	contractBalance = await hre.ethers.provider.getBalance(waveContract.address);
+	/*
+	*コントラクトの残高から0.0001ETH引かれていることを確認
+	*/
+	console.log(
+		"Contract balance:",
+		hre.ethers.utils.formatEther(contractBalance)
+	);
+	
 	let allWaves = await waveContract.getAllWaves();
 	console.log(allWaves);
 }
